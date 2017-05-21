e2cbac8e5bdf30f52dc73f77b97cf70cfca8acf8
Shahidul Islam <shawon_math07@yahoo.com>
2017-05-21 20:36:16 +0600

    6

--- 1.py
+++ 1.py
@@ -1,10 +1,9 @@
 import cv2
+import matplotlib.pyplot as plt
 list1 =[]
 
 img = cv2.imread('image/bar.png')
-
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(gray, 47, 255, cv2.THRESH_BINARY)
+# t = plot.imread('image/bar.png')
 
 C, H, W = img.shape[::-1]
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -14,16 +13,35 @@ _, contours, _ = cv2.findContours(thresh[:, 0:H, ], cv2.RETR_TREE, cv2.CHAIN_APP
 for i in range(len(contours)):
     perimeter = cv2.arcLength(contours[i], True)
     if perimeter >0 and perimeter <80:
-        # cv2.drawContours (img, contours, i, (0, 255, 0), 2)
+        cv2.drawContours (img, contours, i, (0, 255, 0), 1)
         list1.append(i)
 
-for x in list1:
-    M = cv2. moments(contours[x])
-    cx = int(M['m10']/M['m00'])
-    cy = int(M['m01'] / M['m00'])
-    cv2.rectangle(img, (cx-10, cy-10),(cx+10, cy+10), (0, 255, 255), 2)
-cv2.imshow('thresh',img)
-cv2.waitKey(0)
+t = 0
+while t<200:
+    cv2.imwrite('%d.png'%t, img[2:20, t:t+20])
+    t+=20
+
+
+
+
+    # plot.imshow(t)
+# plot.show()
+
+
+
+# for x in list1:
+#     M = cv2. moments(contours[x])
+#     cx = int(M['m10']/M['m00'])
+#     cy = int(M['m01'] / M['m00'])
+#     cv2.rectangle(img, (cx-10, cy-10),(cx+10, cy+10), (0, 255, 255), 2)
+#     cropped = img[cy - 10:cy + 10, cx - 10:cx + 10]
+#     cv2.imshow('cropped', cropped)
+
+
+# h,w = thresh.shape
+# print(w)
+# cv2.imshow('thresh',img)
+# cv2.waitKey(0)
 
 
 
--- 2.py
+++ 2.py
@@ -1,5 +1,6 @@
 import cv2
 list1 = []
+grid1 = []
 
 img = cv2.imread('image/test_image1.jpg')
 
@@ -7,7 +8,7 @@ C, H, W = img.shape[::-1]
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret,thresh = cv2.threshold(gray, 47, 255, cv2.THRESH_BINARY)
 
-_, contours, _ = cv2.findContours(thresh[:, 0:W, ], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+_, contours, _ = cv2.findContours(thresh[:, 0:H, ], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 for i in range(len(contours)):
     perimeter = cv2.arcLength(contours[i], True)
     if perimeter >250 and perimeter <350:
@@ -17,8 +18,18 @@ for x in list1:
     M = cv2. moments(contours[x])
     cx = int(M['m10']/M['m00'])
     cy = int(M['m01'] / M['m00'])
-    cv2.rectangle(img, (cx-40, cy-40),(cx+40, cy+40), (0, 255, 255), 1)
+    # cv2.rectangle(img, (cx-40, cy-40),(cx+40, cy+40), (0, 255, 255), 2)
+    cropped = img[cy-40:cy+40,cx-40:cx+40]
+    # cv2.imshow('cropped', cropped)
+    # w,h,c = cropped.shape
+    for i in range(10):
+        template = cv2.imread('no.'+str(i)+'.png')
+        result = cv2.matchTemplate(cropped, template, cv2.TM_CCOEFF_NORMED)
+        min_val,max_val,min_loc = cv2.minMaxLoc(result)
+        if(max_val>0.9):
+            grid1.append(i)
 
+print(grid1[::-1])
 cv2.imshow('thresh', img)
 cv2.waitKey(0)
 cv2.destroyAllwindows()
